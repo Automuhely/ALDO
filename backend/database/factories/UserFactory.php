981f4f4ddbebd924d326660978e8fa285ed1f2e7
TAlexa1997 <alejandra.marianella.8@gmail.com>
2024-03-20 19:03:18 +0100

    adószám

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -31,8 +31,8 @@ class UserFactory extends Factory
             'szulido' => fake()->date(),
             'szerepkor' =>'ugyfel',
             // generál egy fake 10/11 jegyű számot, az aktuális idő alapján, így nem fordul elő duplikálás
-            'adoazonosito' => floor(time()-9999999999),
-            'adoszam' => floor(time()-99999999999),
+            'adoazonosito' => -1*(floor(time()-9999999999)),
+            'adoszam' => -1*(floor(time()-99999999999)),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
