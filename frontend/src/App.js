033fae9fed3b3f95abe203d4065f09cf8c06f0ec
TAlexa1997 <alejandra.marianella.8@gmail.com>
2024-04-01 17:42:52 +0200

    beimportálás az app.js-be

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Login from "./pages/Login.js";
 import Register from "./pages/Register.js";
 import NoPage from "./pages/NoPage.js";import './App.css';
 import BasicLayout from "./layouts/BasicLayout.js";
+import MunkaAr from "./pages/MunkaAr.js";
 import { AuthProvider } from "./contexts/AuthContext.js";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<NoPage />} />
+        <Route path="/munkaar" element={<MunkaAr/>}/> 
       </Route>
     </Routes>
  //  </BrowserRouter>
