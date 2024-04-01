be1f3874a7aebe0fb1f424d892ab5964d4f73c85
SzluDora <szlucska.dora@gmail.com>
2024-04-02 01:12:16 +0200

    csrf token meta

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
+
 export default axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://localhost:8000/",
   withCredentials: true,
+/*   'X-CSRF-TOKEN'ó: document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+ */
 });
