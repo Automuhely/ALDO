5554ab07ee4e2828262c16acbbb7ce26e5663946
SzluDora <szlucska.dora@gmail.com>
2024-04-02 14:49:36 +0200

    -komment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,6 +3,4 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://localhost:8000/",
   withCredentials: true,
-/*   'X-CSRF-TOKEN'ó: document.querySelector('meta[name="csrf-token"]').getAttribute('content')
- */
 });
