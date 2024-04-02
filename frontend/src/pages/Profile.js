f46ee83886ab9ec3f9a802754dce2bde3cdfb6d8
SzluDora <szlucska.dora@gmail.com>
2024-04-02 17:43:27 +0200

    munkalapjaim táblázat VÁZ

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import useAuthContext, { AuthProvider } from "../contexts/AuthContext";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import axios from "../api/axios";
@@ -15,9 +17,11 @@ export default function Profile() {
   const [szulido, setSzulido] = useState("");
   const [adoszam, setAdoszam] = useState("");
   const [adoazonosito, setAdoazonosito] = useState("");
+  const [szamlaim, setSzamlaim] = useState([{}]);
+  //  const [autoim, setAutoim] = useState({});
   // const [_token, setToken] = useState("");
 
-  const { user, getUser, } = useAuthContext();
+  const { user, getUser, szamlak } = useAuthContext();
 
   useEffect(() => {
     if (!user) {
@@ -30,12 +34,25 @@ export default function Profile() {
       setSzulido(user.szulido);
       setAdoazonosito(user.adoazonosito);
       setAdoszam(user.adoszam);
+
+      // meghívjuk az adatot a backendről külső függvénnyel, aszinkron módon
+      szamlakBetolt();
     }
   }, [user]);
 
+  async function szamlakBetolt() {
+    try {
+      const szamlakData = await szamlak();
+      // beállítjuk a state-t hozzá
+      setSzamlaim(szamlakData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const bekuld = async (e) => {
     e.preventDefault();
-  //  setToken(axios.get("/token"));
+    //  setToken(axios.get("/token"));
     const adat = {
       name: name,
       email: email,
@@ -45,122 +62,166 @@ export default function Profile() {
       adoazonosito: adoazonosito,
       adoszam: adoszam,
       szerepkor: user.szerepkor,
-     // _token: _token,
+      // _token: _token,
     };
 
     console.log(adat);
-
-
-    /* sadasdasdasdasdasdsad */
-
+    /* Beküldés kódja */
   };
 
   return (
     <>
-      <Container className="col-sm-6 m-0">
-        <h1 className="text-center">Profil</h1>
+      <Container className="col-sm-12 m-0">
         {user ? (
-          <Table striped bordered responsive>
-            <thead>
-              <tr>
-                <th colSpan="2" className="text-center">
-                  Személyes adatok
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Név</td>
-                <td>
-                  <input
-                    type="text"
-                    value={name}
-                    onChange={(e) => (
-                      setName(e.target.value), console.log(e.target.value)
-                    )}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>Telefon</td>
-                <td>
-                  <input
-                    type="text"
-                    value={telefon}
-                    onChange={(e) => (
-                      setTelefon(e.target.value), console.log(e.target.value)
-                    )}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>Cím</td>
-                <td>
-                  <input
-                    type="text"
-                    value={cim}
-                    onChange={(e) => (
-                      setCim(e.target.value), console.log(e.target.value)
-                    )}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>Születési idő</td>
-                <td>
-                  <input
-                    type="text"
-                    value={szulido}
-                    onChange={(e) => (
-                      setSzulido(e.target.value), console.log(e.target.value)
-                    )}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>Adóazonosító</td>
-                <td>
-                  <input
-                    type="text"
-                    value={adoazonosito}
-                    onChange={(e) => (
-                      setAdoazonosito(e.target.value),
-                      console.log(e.target.value)
-                    )}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>Adószám</td>
-                <td>
-                  <input
-                    type="text"
-                    value={adoszam}
-                    onChange={(e) => (
-                      setAdoszam(e.target.value), console.log(e.target.value)
-                    )}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>Email</td>
-                <td>
-                  <input
-                    type="text"
-                    value={email}
-                    onChange={(e) => (
-                      setEmail(e.target.value), console.log(e.target.value)
-                    )}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td colSpan={2} className="text-center adatokMenteseGomb">
-                  <Button onClick={bekuld}>Mentés</Button>
-                </td>
-              </tr>
-            </tbody>
-          </Table>
+          <Row>
+          
+            
+            <Col className="col-sm-6 m-0">
+              <h1 className="text-center">Profil</h1>
+              <Table striped bordered responsive>
+                <thead>
+                  <tr>
+                    <th colSpan="2" className="text-center">
+                      Személyes adatok
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Név</td>
+                    <td>
+                      <input
+                        type="text"
+                        value={name}
+                        onChange={(e) => (
+                          setName(e.target.value), console.log(e.target.value)
+                        )}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Telefon</td>
+                    <td>
+                      <input
+                        type="text"
+                        value={telefon}
+                        onChange={(e) => (
+                          setTelefon(e.target.value),
+                          console.log(e.target.value)
+                        )}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Cím</td>
+                    <td>
+                      <input
+                        type="text"
+                        value={cim}
+                        onChange={(e) => (
+                          setCim(e.target.value), console.log(e.target.value)
+                        )}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Születési idő</td>
+                    <td>
+                      <input
+                        type="text"
+                        value={szulido}
+                        onChange={(e) => (
+                          setSzulido(e.target.value),
+                          console.log(e.target.value)
+                        )}
+                      />
+                    </td>
+                  </tr>
+                  {user.adoazonosito ? (
+                    <tr>
+                      <td>Adóazonosító</td>
+                      <td>
+                        <input
+                          type="text"
+                          value={adoazonosito}
+                          onChange={(e) => (
+                            setAdoazonosito(e.target.value),
+                            console.log(e.target.value)
+                          )}
+                        />
+                      </td>
+                    </tr>
+                  ) : (
+                    <tr>
+                      <td>Adószám</td>
+                      <td>
+                        <input
+                          type="text"
+                          value={adoszam}
+                          onChange={(e) => (
+                            setAdoszam(e.target.value),
+                            console.log(e.target.value)
+                          )}
+                        />
+                      </td>
+                    </tr>
+                  )}
+
+                  <tr>
+                    <td>Email</td>
+                    <td>
+                      <input
+                        type="text"
+                        value={email}
+                        onChange={(e) => (
+                          setEmail(e.target.value), console.log(e.target.value)
+                        )}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan={2} className="text-center adatokMenteseGomb">
+                      <Button onClick={bekuld}>Mentés</Button>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Col>
+            <Col className="col-sm-12 m-auto">
+              <h1 className="text-center">Autóim</h1>
+
+              
+              <Table bordered hover responsive>
+                <thead>
+                  <tr>
+                    {/* Munkalapszám helyett számlaszám kellene */}
+                    <th>Munkalapszám</th>
+                    {/* rendszám, becenév átláthatóbb lenne */}
+                    <th>Autó</th>
+                    <th>Munkavezető</th>
+                    <th>Leírás</th>
+                    <th>Módosult</th>
+                    <th colSpan={2}>Módosítás</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.values(szamlaim).map((e, i) => {
+                    return (
+                      <tr key={i}>
+                        <td>{e.munkalapszam}</td>
+                        <td>{e.auto}</td>
+                        <td>{e.munkavezeto}</td>
+                        <td>{e.altalanosLeiras}</td>
+                        <td>{e.updated_at}</td>
+                        <td>Megtekint</td>
+                        <td>Letölt</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
         ) : (
           <div>Valami hiba történt, jelentkezz be újra!</div>
         )}
