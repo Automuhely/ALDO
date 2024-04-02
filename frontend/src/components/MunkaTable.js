d8eca40849b1efd7fa7bbe37b8316b8f27190100
SzluDora <szlucska.dora@gmail.com>
2024-04-02 21:10:08 +0200

    dekor

--- frontend/src/components/MunkaTable.js
+++ frontend/src/components/MunkaTable.js
@@ -1,46 +1,58 @@
-import React, { useMemo } from 'react';
-import { useTable } from 'react-table';
+import React, { useEffect, useMemo } from "react";
+import { useTable } from "react-table";
+import useAuthContext from "../contexts/AuthContext";
 
-export default  function MunkaTable({ munkak }) {
+export default function MunkaTable({ munkak }) {
+
   const columns = useMemo(
     () => [
       {
-        Header: 'Munka megnevezése',
-        accessor: 'megnevezes', 
+        Header: "Munka megnevezése",
+        accessor: "megnevezes",
       },
       {
-        Header: 'Munka ára',
-        accessor: 'ara', 
+        Header: "Munka ára",
+        accessor: "ara",
         Cell: ({ value }) => `${value} Ft`,
       },
     ],
     []
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data: munkak });
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data: munkak });
 
   return (
-    <table {...getTableProps()} style={{ border: '1px solid black', borderCollapse: 'collapse' }}>
+    <table
+      {...getTableProps()}
+      style={{ border: "1px solid black", borderCollapse: "collapse" }}
+    >
       <thead>
-        {headerGroups.map(headerGroup => (
+        {headerGroups.map((headerGroup) => (
           <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()} style={{ border: '1px solid black', padding: '8px' }}>
-                {column.render('Header')}
+            {headerGroup.headers.map((column) => (
+              <th
+                {...column.getHeaderProps()}
+                style={{ border: "1px solid black", padding: "8px" }}
+              >
+                {column.render("Header")}
               </th>
             ))}
           </tr>
         ))}
       </thead>
       <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
+        {rows.map((row) => {
           prepareRow(row);
           return (
             <tr {...row.getRowProps()} key={row.id}>
-              {row.cells.map(cell => {
+              {row.cells.map((cell) => {
                 return (
-                  <td {...cell.getCellProps()} style={{ border: '1px solid black', padding: '8px' }}>
-                    {cell.render('Cell')}
+                  <td
+                    {...cell.getCellProps()}
+                    style={{ border: "1px solid black", padding: "8px" }}
+                  >
+                    {cell.render("Cell")}
                   </td>
                 );
               })}
@@ -51,5 +63,3 @@ export default  function MunkaTable({ munkak }) {
     </table>
   );
 }
-
-
