177a9311647a3ef4e8a3ae0b8091c7e4433b768f
SzluDora <szlucska.dora@gmail.com>
2024-04-07 17:54:21 +0200

    munkalap/munkalaptétel observer, kivételek dobása (ügyfél != munkavezető, munkalaptétel nem lehet a munkalapon kétszer)

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -7,21 +7,49 @@ use App\Models\Feladat;
 use App\Models\Munkalap;
 use App\Models\MunkalapTetel;
 use App\Models\User;
+use Exception;
+use GuzzleHttp\Psr7\Message;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-         User::factory(10)->create();   
-         Auto::factory(10)->create();
-         Munkalap::factory(10)->create();
-         Feladat::factory(10)->create();
-         MunkalapTetel::factory(10)->create();
-       //  $this->call(MunkaArSeeder::class);
-    }
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+
+
+    User::factory(10)->create();
+    Auto::factory(10)->create();
+
+    $maxProbalkozasSzama = 3;
+    $probalkozas = 0;
+
+    do {
+      try {
+        Munkalap::factory(10)->create();
+        MunkalapTetel::factory(10)->create();
+        Feladat::factory(10)->create();
+
+        // ha sikeres kilép
+        break;
+      } catch (Exception $e) {
+        if ($e->getMessage()) {
+          dd($e->getMessage());
+        } else {
+          dd($e);
+        }
+        $probalkozas++;
+
+        if ($probalkozas >= $maxProbalkozasSzama) {
+          dd("nem sikerült");
+          break;
+        }
+      }
+    } while (true);
+
+    //  $this->call(MunkaArSeeder::class);
+  }
 }
