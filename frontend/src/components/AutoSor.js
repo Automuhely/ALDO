b0deb7faabcadab5e81448a964d43e4a124c0567
SzluDora <szlucska.dora@gmail.com>
2024-04-07 12:37:20 +0200

    szerkeszthetőség követése usaState-vel

--- frontend/src/components/AutoSor.js
+++ frontend/src/components/AutoSor.js
@@ -1,73 +1,67 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import axios from "../api/axios";
+import { useState } from "react";
 
+export default function AutoSor(props) {
+  const autoid = props.auto.id;
+  const [szerkesztheto, setSzerkesztheto] = useState(false);
 
-export default function AutoSor(props){
-    const autoid = props.auto.id
-    let szerkNyitva = 0;
+  function szerkeszt() {
+    setSzerkesztheto(!szerkesztheto);
+  }
 
-    function szerkeszt(e){
-        if(!szerkNyitva){
-            szerkNyitva = 1
-            let elsoGomb = document.getElementById(`elsoGomb-${autoid}`);
-            elsoGomb.innerHTML="Mentés"
-            console.log(elsoGomb)
-            elsoGomb.classList.toggle("btn-primary")
-            console.log(elsoGomb)
-
-            elsoGomb.classList.toggle("btn-success")
-            console.log(elsoGomb)
-
-
-            let masodikGomb = document.getElementById(`masodikGomb-${autoid}`);
-            masodikGomb.innerHTML="Mégse"
-        /*     masodikGomb.classList.toggle("btn-danger")
-            masodikGomb.classList.toggle("btn-primary") */
-        }else{
-            szerkNyitva = 0
-            let elsoGomb = document.getElementById(`elsoGomb-${autoid}`);
-            elsoGomb.innerHTML="Szerkeszt"
-            console.log("ITT KELLENE KÉKNEK LENNI")
-            console.log(elsoGomb)
-
-            elsoGomb.classList.toggle("btn-success")
-            console.log(elsoGomb)
-
-            elsoGomb.classList.toggle("btn-primary")
-            console.log(elsoGomb)
-            
-            let masodikGomb = document.getElementById(`masodikGomb-${autoid}`);
-            masodikGomb.innerHTML="Töröl"
-            /* masodikGomb.classList.toggle("btn-danger")
-            masodikGomb.classList.toggle("btn-primary") */
-
-        }
-    }
-
-    return(
-        <tr id={"sor"+props.auto.id}>
-        <td>
-          <Form.Control type="text" disabled value={props.auto.becenev??""} onChange={()=>{props.setBecenev(props.auto.target.value)}}/>
-        </td>
-        <td>
-          <Form.Control type="text" disabled value={props.auto.rendszam??""} onChange={()=>{props.setRendszam(props.auto.target.value)}}/>
-        </td>
-        <td>
-          <Form.Control type="text" disabled value={props.auto.alvazszam??""} onChange={()=>{props.setAlvazszam(props.auto.target.value)}}/>
-        </td>
-        <td>
-          <Form.Control type="text" disabled value={props.auto.motorkod??""} onChange={()=>{props.setMotorkod(props.auto.target.value)}}/>
-        </td>
-        <td>
-          <Form.Control type="text" disabled value={props.auto.evjarat??""} onChange={()=>{props.setEvjarat(props.auto.target.value)}}/>
-        </td>
-        <td>
-          <Button variant="primary" autoid={`elso${autoid??""}`} id={`elsoGomb-${autoid??""}`}  className="autoimElsoGomb" onClick={(e)=>{szerkeszt(e)}}>Szerkeszt</Button>
-        </td>
-        <td>
-          <Button variant="danger"  autoid={`masodik${autoid??""}`} id={`masodikGomb-${autoid??""}`}  className="autoimMasodikGomb" onClick={(e)=>{szerkeszt(e)}}>Töröl</Button>
-        </td>
-      </tr>
-
-    )
+  return (
+    <tr id={"sor" + props.auto.id}>
+      <td>
+        <Form.Control
+          className="becenev"
+          type="text"
+          disabled={!szerkesztheto}
+          value={props.auto.becenev ?? ""}
+          onChange={(e) => {
+            props.setBecenev(e.target.value);
+          }}
+        />
+      </td>
+      <td>
+        <Form.Control type="text" disabled value={props.auto.rendszam ?? ""} />
+      </td>
+      <td>
+        <Form.Control type="text" disabled value={props.auto.alvazszam ?? ""} />
+      </td>
+      <td>
+        <Form.Control type="text" disabled value={props.auto.motorkod ?? ""} />
+      </td>
+      <td>
+        <Form.Control type="text" disabled value={props.auto.evjarat ?? ""} />
+      </td>
+      <td>
+        <Button
+          variant={szerkesztheto ? "success" : "primary"}
+          autoid={`elso${autoid ?? ""}`}
+          id={`elsoGomb-${autoid ?? ""}`}
+          className="autoimElsoGomb"
+          onClick={(e) => {
+            szerkeszt(e);
+          }}
+        >
+          {szerkesztheto ? "Mentés" : "Szerkeszt"}
+        </Button>
+      </td>
+      <td>
+        <Button
+          variant="danger"
+          autoid={`masodik${autoid ?? ""}`}
+          id={`masodikGomb-${autoid ?? ""}`}
+          className="autoimMasodikGomb"
+          onClick={(e) => {
+            szerkeszt(e);
+          }}
+        >
+          Töröl
+        </Button>
+      </td>
+    </tr>
+  );
 }
