1706d7e424d9e3c0214559ff651739adfd094d3f
SzluDora <szlucska.dora@gmail.com>
2024-04-07 10:32:24 +0200

    401, 422-es hibák lekezelése, hibaüzenet ignorálása (return null)

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -1,86 +1,125 @@
 import { createContext, useContext, useState } from "react";
-import axios from "../api/axios";
+import axios from "../api/axios.js";
 import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
+// *********************************************
+// **                                         **
+// **               IMPORTANT!                **
+// **            Delete or modify             **
+// **        the elements marked below        **
+// **                                         **
+// *********************************************
+
 export const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
+
   const [user, setUser] = useState(null);
-  const [tokenem, setTokenem] = useState("");
   const [errors, setErrors] = useState({
     name: "",
     email: "",
     password: "",
     password_confirmation: "",
-    cim: "",
-    telefon: "",
-    adoszam: "",
-    adoazonosito: "",
-    szulido: "",
   });
-  //const csrf = () => axios.get("/sanctum/csrf-cookie");
+
   let token = "";
-  const csrf = () =>
-    axios.get("/token").then((response) => {
-      // console.log("Token lekérés response: ", response);
+  const csrf = async () => {
+    try {
+      const response = await axios.get("/token");
+      console.log(response);
       token = response.data;
-
-      setTokenem(response.data);
-      //console.log("AuthContext Token: ", response.data);
-      return tokenem;
-    });
-
-  //bejelentkezett felhasználó adatainak lekérdezése
-  const getUser = async () => {
-    const { data } = await axios.get("/api/user");
-    setUser(data);
+      return response.data;
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        console.log("401, nem tudtam token-t lekérdezni", error);
+      } else {
+        console.error("Hiba történt a token lekérése során", error);
+      }
+      // REMOVE IT WHEN YOU TESTING
+      return null;
+      //throw error;
+    }
   };
-  const logout = async () => {
-    await csrf();
-    console.log(token);
-    axios.post("/logout", { _token: token }).then((resp) => {
-      setUser(null);
-      console.log(resp);
-    });
+
+  const getUser = async () => {
+    try {
+      const { data } = await axios.get("/api/user");
+      setUser(data);
+    } catch (error) {
+      if (error.response && error.response.status === 422) {
+        console.error("422-es hiba történt:", error);
+        setErrors(error.response.data.errors);
+      }
+      if (error.response && error.response.status === 401) {
+        console.log("401, nem történt bejelenkezés", error);
+      } else {
+        console.error("Egyéb hiba történt:", error);
+      }
+      // REMOVE IT WHEN YOU TESTING
+      return null;
+      // throw error;
+    }
   };
 
   const loginReg = async ({ ...adat }, vegpont) => {
-    await csrf();
-    console.log(token);
-    adat._token = token;
-    console.log(adat);
-    //lekérjük a csrf tokent
-    //await csrf();
-    //bejelentkezés
-    //Összegyűjtjük egyetlen objektumban az űrlap adatokat
-
-    // Megrpóbáljuk elküldeni a /login végpontra az adatot
-    // hiba esetén kiiratjuk a hibaüzenetet
     try {
+      await csrf();
+      console.log(token);
+      adat._token = token;
+      console.log(adat);
       await axios.post(vegpont, adat);
-      console.log("siker");
-      //sikeres bejelentkezés/regisztráció esetén
-      //Lekérdezzük a usert
+      console.log("Sikeres bejelentkezés/regisztráció");
       await getUser();
-      //elmegyünk  a kezdőlapra
-      navigate("/profil");
+      navigate("/profile");
     } catch (error) {
-      console.log(error);
-      if (error.response.status === 422) {
+      if (error.response && error.response.status === 422) {
+        console.error("422-es hiba történt:", error);
         setErrors(error.response.data.errors);
       }
+      if (error.response && error.response.status === 401) {
+        console.log("401, nem történt bejelenkezés", error);
+      } else {
+        console.error("Egyéb hiba történt:", error);
+      }
+      // REMOVE IT WHEN YOU TESTING
+      return null;
+      // throw error;
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await csrf();
+      console.log(token);
+      const resp = await axios.post("/logout", { _token: token });
+      navigate("/");
+      console.log(resp);
+    } catch (error) {
+      if (error.response && error.response.status === 422) {
+        console.error("422-es hiba történt:", error);
+        setErrors(error.response.data.errors);
+      }
+      if (error.response && error.response.status === 401) {
+        console.log("401, nem történt bejelenkezés", error);
+      } else {
+        console.error("Egyéb hiba történt:", error);
+      }
+      // REMOVE IT WHEN YOU TESTING
+      return null;
+      // throw error;
+    } finally {
+      setUser(null);
     }
   };
 
   return (
-    <AuthContext.Provider
-      value={{ logout, loginReg, errors, getUser, user, csrf }}
-    >
+    <AuthContext.Provider value={{ loginReg, logout, errors, getUser, user }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
 export default function useAuthContext() {
   return useContext(AuthContext);
 }
