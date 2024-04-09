b4eb65cb15018fb8f74643ac897298f9745cb18f
SzluDora <szlucska.dora@gmail.com>
2024-04-09 16:42:52 +0200

    magyarítás

---
+++ backend/lang/hu/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'failed' => 'Ezek az adatok nem egyeznek a nyilvántartásainkkal.',
+    'password' => 'A megadott jelszó helytelen.',
+    'throttle' => 'Túl sok bejelentkezési kísérlet. Kérjük, próbálkozzon újra :seconds másodperc múlva.',
+
+];
