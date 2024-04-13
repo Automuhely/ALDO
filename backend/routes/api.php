7846c3ac5820084477bde04da5370122b2e7add5
SzluDora <szlucska.dora@gmail.com>
2024-04-13 15:30:03 +0200

    /user szükséges a bejelentkezéshez!

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,12 +10,9 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/* Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-
 });
- */
 
 Route::middleware(['auth:sanctum'])->group(function () {
 
