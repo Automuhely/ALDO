ed5fc7e0ee2355093dc2f7bd1508facc5b29fcb3
TAlexa1997 <alejandra.marianella.8@gmail.com>
2024-04-16 21:36:46 +0200

    munkafolyamatok oldal

--- frontend/src/pages/MunkaFolyamatok.js
+++ frontend/src/pages/MunkaFolyamatok.js
@@ -1,9 +1,15 @@
-import { useEffect } from "react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../api/axios";
 import useAuthContext from "../contexts/AuthContext";
+import MunkaFolyTable from "../components/MunkaFolyTable";
+import MunkaElNemKezdettTable from "../components/MunkaElNemKezdettTable";
+import MunkaBefejezettTable from "../components/MunkaBefejezettTable";
 
 export default function MunkaFolyamatok() {
-  const { user, getUser } = useAuthContext();
+  const { user, getUser,moveToFinished,moveToStarted,csrf } = useAuthContext();
+  const [ElKezdettMunkak, setElKezdettMunkak] = useState([]);
+  const [ElNemKezdettMunkak, setElNemKezdettMunkak] = useState([]);
+  const [BefejezettMunkak, setBefejezettMunkak] = useState([]);
 
   useEffect(() => {
     if (!user) {
@@ -11,10 +17,50 @@ export default function MunkaFolyamatok() {
     }
   }, [user]);
 
+ 
+
+  useEffect(() => {
+    axios.get("/api/folyamatmunka")
+      .then(response => {
+        setElKezdettMunkak(response.data);
+      })
+      .catch(error => {
+        console.error("Hiba történt az elkezdett munkák lekérésekor:", error);
+      });
+  }, []);
+
+
+  useEffect(() => {
+    axios.get("/api/befejezettmunka")
+      .then(response => {
+        setBefejezettMunkak(response.data);
+      })
+      .catch(error => {
+        console.error("Hiba történt a befejezett munkák lekérésekor::", error);
+      });
+  }, []);
+
+
+  useEffect(() => {
+    axios.get("/api/elnemkezdetmunka")
+      .then(response => {
+        setElNemKezdettMunkak(response.data);
+      })
+      .catch(error => {
+        console.error("Hiba történt az el nem kezdett munkák lekérésekor:", error);
+      });
+  }, []);
+
+
   return (
     <div>
-        <h1>Munkák</h1>
-        
+      <h1>Munkafolyamatok</h1>
+      <MunkaElNemKezdettTable
+        ElNemKezdettMunkak={ElNemKezdettMunkak}
+        onMoveToStarted={moveToStarted}
+      />
+      <MunkaFolyTable ElKezdettMunkak={ElKezdettMunkak}  onMoveToFinished={moveToFinished}/>
+      <MunkaBefejezettTable BefejezettMunkak={BefejezettMunkak} />
     </div>
   );
-  }
+}
