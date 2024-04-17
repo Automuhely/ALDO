fc86cd5a1f22b6eb01161fbbf0bd785bf5823e22
SzluDora <szlucska.dora@gmail.com>
2024-04-17 22:16:23 +0200

    proba context

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
 import useAuthContext from "../contexts/AuthContext";
+import useThemeContext from "../contexts/ThemeContext";
 
 export default function Home() {
   const { user, getUser } = useAuthContext();
+  const { setDarkTheme, darkTheme } = useThemeContext();
 
   useEffect(() => {
     console.log(user);
@@ -13,7 +15,17 @@ export default function Home() {
 
   return (
     <>
-      <div>Főoldal</div>
+      <div className="text-center" style={darkTheme}>
+        <div>Főoldal</div>
+        <button
+          onClick={() => {
+            setDarkTheme((prevState) => !prevState);
+          }}
+        >
+          Nyomd meg
+        </button>
+        <div>Ez itt egy doboz egy szöveggel.</div>
+      </div>
       {/* <p>Bejelentkezett felhasználó: {user?.name}</p> */}
     </>
   );
