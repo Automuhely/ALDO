6d9866a252a8e37ea5a6420479b4007134a462c2
SzluDora <szlucska.dora@gmail.com>
2024-04-17 22:14:35 +0200

    dekor

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,7 +2,6 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import useAuthContext from "../contexts/AuthContext";
