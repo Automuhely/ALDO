d861aef1f2727d88ce5835ea0d6145686957f26c
SzluDora <szlucska.dora@gmail.com>
2024-04-20 21:39:46 +0200

    becenév szerkesztés jav

--- frontend/src/components/AutoSor.js
+++ frontend/src/components/AutoSor.js
@@ -1,99 +1,69 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import axios from "../api/axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import useAuthContext from "../contexts/AuthContext";
 
 export default function AutoSor(props) {
   const [beceneve, setBeceneve] = useState(props.auto.becenev ?? "");
-  const [szerkesztheto, setSzerkesztheto] = useState(false);
+  const [kuldheto, setKuldheto] = useState(false);
 
   const autoid = props.auto.id;
   const vegpont = "api/autos/" + autoid;
   const { csrf } = useAuthContext();
 
   async function szerkeszt(e) {
-    setSzerkesztheto((prevState) => !prevState);
-    if (szerkesztheto) {
-      const token = await csrf();
+    const token = await csrf();
+    if (kuldheto === true) {
       try {
-        await axios
-          .put(vegpont, { becenev: beceneve, _token: token })
-          .then((response) => {
-            console.log(response);
-          })
-          .catch((error) => {
-            console.error("Hiba történt:", error);
-          });
+        console.log("becenév változtatás")
+        await axios.put(vegpont, { becenev: beceneve, _token: token });
       } catch (error) {
         console.error("Hiba történt:", error);
       }
     }
+    setKuldheto((prevState) => !prevState);
   }
-
   return (
     <tr id={"sor" + props.auto.id}>
       <td>
         <Form.Control
           className="becenev"
           type="text"
-          disabled={!szerkesztheto}
+          disabled={!kuldheto}
           defaultValue={props.auto.becenev ?? ""}
           onChange={(e) => {
             setBeceneve(e.target.value);
           }}
         />
+        <span>
+          <Button
+            variant={kuldheto ? "success" : "primary"}
+            style={{
+              width: "2em",
+              fontSize: "1.5em",
+              margin: "0",
+              padding: "0",
+            }}
+            autoid={`elso${autoid ?? ""}`}
+            id={`elsoGomb-${autoid ?? ""}`}
+            className="autoimElsoGomb"
+            onClick={(e) => {
+              szerkeszt(e);
+            }}
+          >
+            {kuldheto ? (
+              <i className="fa-solid fa-floppy-disk"></i>
+            ) : (
+              <i className="fa-regular fa-pen-to-square"></i>
+            )}
+          </Button>
+        </span>
       </td>
-      <td>
-        <Form.Control
-          type="text"
-          disabled
-          readOnly
-          value={props.auto.rendszam ?? ""}
-        />
-      </td>
-      <td>
-        <Form.Control
-          type="text"
-          disabled
-          readOnly
-          value={props.auto.alvazszam ?? ""}
-        />
-      </td>
-      <td>
-        <Form.Control
-          type="text"
-          disabled
-          readOnly
-          value={props.auto.motorkod ?? ""}
-        />
-      </td>
-      <td>
-        <Form.Control
-          type="text"
-          disabled
-          readOnly
-          value={props.auto.evjarat ?? ""}
-        />
-      </td>
-      <td>
-        <Button
-          variant={szerkesztheto ? "success" : "primary"}
-          style={{ width: "2em", fontSize: "1.5em", margin: "0", padding: "0" }}
-          autoid={`elso${autoid ?? ""}`}
-          id={`elsoGomb-${autoid ?? ""}`}
-          className="autoimElsoGomb"
-          onClick={(e) => {
-            szerkeszt(e);
-          }}
-        >
-          {szerkesztheto ? (
-            <i className="fa-solid fa-floppy-disk"></i>
-          ) : (
-            <i className="fa-regular fa-pen-to-square"></i>
-          )}
-        </Button>
-      </td>
+      <td>{props.auto.rendszam ?? ""}</td>
+      <td>{props.auto.alvazszam ?? ""}</td>
+      <td>{props.auto.motorkod ?? ""}</td>
+      <td>{props.auto.evjarat ?? ""}</td>
     </tr>
   );
 }
