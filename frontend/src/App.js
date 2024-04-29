e9a5e5e49ed57c8766cfb1656e7b2fa2d062f8af
TAlexa1997 <alejandra.marianella.8@gmail.com>
2024-04-29 20:47:21 +0200

    további oldalak route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,8 @@ import Galeria from "./pages/Galeria.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MunkaFolyamatok from "./pages/MunkaFolyamatok.js";
 import UserProfile from "./pages/UserProfile.js";
+import AkummulatorCsere from "./pages/szolgaltatasok/AkkumulatorCsere.js";
+import Futomu from "./pages/szolgaltatasok/Futomu.js";
 
 function App() {
   return (
@@ -20,18 +22,17 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profil" element={<UserProfile />} />
-        <Route path="/munkafolyamatok" element={<MunkaFolyamatok/>}/>
+        <Route path="/munkafolyamatok" element={<MunkaFolyamatok />} />
         <Route path="/register" element={<Register />} />
         <Route path="/munkaar" element={<MunkaAr />} />
         <Route path="/kapcsolat" element={<Email />} />
         <Route path="/galeria" element={<Galeria />} />
+        <Route path="/akkumulatorcsere" element={<AkummulatorCsere />} />
+        <Route path="/futomu" element={<Futomu />} />
         <Route path="*" element={<NoPage />} />
       </Route>
     </Routes>
     //  </BrowserRouter>
-
-    
-      
   );
 }
 
