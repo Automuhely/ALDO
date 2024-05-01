d2f93675d83fcb7dd5a165fa840559f4528ea255
TAlexa1997 <alejandra.marianella.8@gmail.com>
2024-05-01 11:33:30 +0200

    Galéria feltöltése

--- frontend/src/pages/Galeria.js
+++ frontend/src/pages/Galeria.js
@@ -26,13 +26,28 @@ const photos = [
   { src: "/img/munkadijak.jpg", width: 1600, height: 1600 },
   { src: "/img/muszaki vizsga.jpg", width: 1200, height: 900 },
   { src: "/img/olajcsere.jpg", width: 1600, height: 1200 },
+  { src: "/img/1.jpg", width: 1600, height: 1200 },
+  { src: "/img/2.jpg", width: 1200, height: 900 },
+  { src: "/img/3.jpg", width: 800, height: 600 },
+  { src: "/img/4.jpg", width: 1600, height: 1600 },
+  { src: "/img/5.jpg", width: 800, height: 600 },
+  { src: "/img/6.jpg", width: 1200, height: 900 },
+  { src: "/img/7.jpg", width: 1600, height: 1200 },
+  { src: "/img/8.jpg", width: 1600, height: 1200 },
+  { src: "/img/9.jpg", width: 1400, height: 900 },
+  { src: "/img/10.jpg", width: 1400, height: 1000 },
+  { src: "/img/11.jpg", width: 1600, height: 900 },
+  { src: "/img/12.jpg", width: 1800, height: 900 },
+  { src: "/img/13.jpg", width: 1600, height: 600 },
+  { src: "/img/14.jpg", width: 1400, height: 900 },
+  { src: "/img/15.jpg", width: 1600, height: 1200 },
   
 ];
 
   return (
     
       <Container style={{ margin:"3rem" }}>
-        <PhotoAlbum layout="columns" photos={photos} />
+        <PhotoAlbum layout="rows" photos={photos} />
       </Container>
     
   );
