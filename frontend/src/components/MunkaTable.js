ab3b648374f9f77726eb80b2240040c6646b92f3
TAlexa1997 <alejandra.marianella.8@gmail.com>
2024-05-01 11:37:40 +0200

    munka ár táblázat müködő törlés gomb

--- frontend/src/components/MunkaTable.js
+++ frontend/src/components/MunkaTable.js
@@ -1,10 +1,17 @@
 import React, { useMemo } from "react";
+import { Button, Table } from "react-bootstrap";
 import { useTable } from "react-table";
+import useAuthContext from "../contexts/AuthContext";
+import axios from "../api/axios";
 
 export default function MunkaTable({ munkak }) {
-
+  const { csrf,Torles} = useAuthContext();
   const columns = useMemo(
     () => [
+      {
+        Header: "id",
+        accessor: "id",
+      },
       {
         Header: "Munka megnevezése",
         accessor: "megnevezes",
@@ -14,17 +21,63 @@ export default function MunkaTable({ munkak }) {
         accessor: "ara",
         Cell: ({ value }) => `${value} Ft`,
       },
+      {
+        Header: " ",
+        accessor: "Szerkesztés",
+        Cell: ({ row }) => (
+          <Button variant="primary" onClick={() => szerkesztesgomb(row.original.id)}>
+            Szerkesztés
+          </Button>
+        ),
+      },
+      {
+        Header: " ",
+        accessor: "Törlés",
+        Cell: ({ row }) => (
+          <Button variant="primary" onClick={() => torlesgomb(row.original.id)}>
+            Törlés
+          </Button>
+        ),
+      },
     ],
     []
   );
+
+  const szerkesztesgomb = async (id) => {
+    try {
+      const token = await csrf();
+      const data = { id, _token: token };
+      console.log("Munka megnevezése:",id)
+      const response = await axios.put("/api/arak/{id}", data);
+      console.log("Sikeres munka ár szerkesztése!");
+      alert("Sikeres munka ár szerkesztése!");
+    } catch (error) {
+      console.error("Hiba történt a státusz megváltoztatása közben:", error);
+    }
+  };
+
+  const torlesgomb = async (id) => {
+    try {
+        const token = await csrf();
+        const data = { id,_token: token };
+        console.log("Munka ár id:", id);
+        const response = await axios.delete(`/api/arak/${id}`, { data }); 
+        console.log("Sikeres munka ár törlése!");
+        window.location.reload()
+        alert("Sikeres munka ár törlése!");
+    } catch (error) {
+        console.error("Hiba történt a munka törlésekor:", error);
+    }
+};
+
 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({ columns, data: munkak });
 
   return (
-    <table
+    <Table
       {...getTableProps()}
-      style={{ border: "1px solid black", borderCollapse: "collapse" }}
+      style={{ border: "1px solid black", borderCollapse: "collapse" ,textAlign:"center"}}
     >
       <thead>
         {headerGroups.map((headerGroup) => (
@@ -59,6 +112,6 @@ export default function MunkaTable({ munkak }) {
           );
         })}
       </tbody>
-    </table>
+    </Table>
   );
 }
--- frontend/src/pages/MunkaAr.js
+++ frontend/src/pages/MunkaAr.js
@@ -1,34 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import axios from '../api/axios'; 
-import MunkaTable from '../components/MunkaTable';
-import useAuthContext from '../contexts/AuthContext';
+import React, { useState, useEffect } from "react";
+import axios from "../api/axios";
+import MunkaTable from "../components/MunkaTable";
+import useAuthContext from "../contexts/AuthContext";
+import { Container } from "react-bootstrap";
 
-export default function MunkaAr(){
-    const [munkak, setMunkak] = useState([]);
-    const { user, getUser } = useAuthContext();
+export default function MunkaAr() {
+  const [munkak, setMunkak] = useState([]);
+  const { user, getUser } = useAuthContext();
 
-    useEffect(() => {
-      if (!user) {
-        getUser();
+  useEffect(() => {
+    if (!user) {
+      getUser();
+    }
+  }, [user]);
+
+  useEffect(() => {
+    async function fetchMunkak() {
+      try {
+        const response = await axios.get("/api/arak");
+        setMunkak(response.data);
+      } catch (error) {
+        console.error("Hiba történt a munkadíjak lekérésekor:", error);
       }
-    }, [user]);
+    }
+    fetchMunkak();
+  }, []);
 
-    useEffect(() => {
-        async function fetchMunkak() {
-            try {
-                const response = await axios.get('/api/arak'); 
-                setMunkak(response.data); 
-            } catch (error) {
-                console.error('Hiba történt a munkadíjak lekérésekor:', error);
-            }
-        }
-      fetchMunkak();
-    }, []);
-  
-    return (
-      <div>
-        <h1>Munkadíjak</h1>
+  return (
+    <div>
+      <Container>
+        <h1 style={{ textAlign:"center", paddingTop:"30px",paddingBottom:"30px" }}>Munkadíjak</h1>
         <MunkaTable munkak={munkak} />
-      </div>
-    );
+      </Container>
+    </div>
+  );
 }
