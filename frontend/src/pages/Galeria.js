d7b321f614d56ccb3217f7db252d8b4f8f015e1e
TAlexa1997 <alejandra.marianella.8@gmail.com>
2024-05-01 11:08:36 +0200

    Minimális galéria létrehozása

--- frontend/src/pages/Galeria.js
+++ frontend/src/pages/Galeria.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import useAuthContext from "../contexts/AuthContext";
-import { Card } from "react-bootstrap";
+import PhotoAlbum from "react-photo-album";
+import { Container } from "react-bootstrap";
+
 
 export default function Galeria() {
   const { user, getUser } = useAuthContext();
@@ -12,12 +14,26 @@ export default function Galeria() {
   }, [user]);
 
   
+const photos = [
+  { src: "/img/Akkumulator_csere.jpg", width: 1400, height: 1000 },
+  { src: "/img/atvizsgalas.jpg", width: 1600, height: 900 },
+  { src: "/img/fekcsere.jpg", width: 1800, height: 900 },
+  { src: "/img/futomubeallítas.jpg", width: 1600, height: 600 },
+  { src: "/img/gumicsere.jpg", width: 1400, height: 900 },
+  { src: "/img/időszakos szerviz.jpg", width: 1600, height: 1200 },
+  { src: "/img/kipufogó.jpg", width: 800, height: 600 },
+  { src: "/img/Klíma.jpg", width: 1600, height: 900 },
+  { src: "/img/munkadijak.jpg", width: 1600, height: 1600 },
+  { src: "/img/muszaki vizsga.jpg", width: 1200, height: 900 },
+  { src: "/img/olajcsere.jpg", width: 1600, height: 1200 },
+  
+];
 
   return (
-    <div>
-      <div className="galery">
-      <Card.Img src="img/muszaki vizsga.jpg" alt="Card image" />
-      </div>
-    </div>
+    
+      <Container style={{ margin:"3rem" }}>
+        <PhotoAlbum layout="columns" photos={photos} />
+      </Container>
+    
   );
 }
